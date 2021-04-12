3a6848cf0f10324e8df748c44a912d17a30641b6
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-12 15:58:01 +0200

    :sparkles: User can click choices

--- src/presentation/controllers/index.ts
+++ src/presentation/controllers/index.ts
@@ -2,5 +2,5 @@ import { Application } from "express";
 import { navigateSupportFlow } from "./support-flow.controller";
 
 export const attachControllersToApp = (app: Application): void => {
-    app.get("/", navigateSupportFlow);
+    app.get("/(*)?", navigateSupportFlow);
 };
--- src/presentation/controllers/support-flow.controller.ts
+++ src/presentation/controllers/support-flow.controller.ts
@@ -1,12 +1,28 @@
 import { Request, Response } from "express";
 import { navigateSupportFlowUseCase } from "../../application/usecases/navigate-support-flow.usecase";
 import { ChoiceId } from "../../domain/support/question-tree";
+import { UserChoices } from "../../domain/support/support-flow";
 import { questionTree } from "../../infrastructure/config";
 
 export const navigateSupportFlow = (req: Request, res: Response) => {
-    const supportFlow = navigateSupportFlowUseCase(questionTree, [
-        "choice1_1" as ChoiceId,
-    ]);
+    const userChoices = req.path
+        .split("/")
+        .filter((chunck) => chunck !== "") as UserChoices;
 
-    res.render("index.nj", { supportFlow });
+    const supportFlow = navigateSupportFlowUseCase(questionTree, userChoices);
+
+    const choicesPrefixes = userChoices.reduce(
+        (choicesPrefixes: string[], userChoice, index) => {
+            if (index === 0) {
+                return [userChoice];
+            }
+            return [
+                ...choicesPrefixes,
+                choicesPrefixes[choicesPrefixes.length - 1] + "/" + userChoice,
+            ];
+        },
+        []
+    );
+
+    res.render("index.nj", { supportFlow, choicesPrefixes });
 };
