7f25f9d6e4f03ec7fa75904f3b54d751dbc795f4
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-12 15:03:31 +0200

    :clown_face: Mock user choice

--- src/presentation/controllers/support-flow.controller.ts
+++ src/presentation/controllers/support-flow.controller.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
 import { navigateSupportFlowUseCase } from "../../application/usecases/navigate-support-flow.usecase";
+import { ChoiceId } from "../../domain/support/question-tree";
 import { questionTree } from "../../infrastructure/config";
 
 export const navigateSupportFlow = (req: Request, res: Response) => {
-    const supportFlow = navigateSupportFlowUseCase(questionTree, []);
+    const supportFlow = navigateSupportFlowUseCase(questionTree, [
+        "choice1_1" as ChoiceId,
+    ]);
 
     res.render("index.nj", { supportFlow });
 };
