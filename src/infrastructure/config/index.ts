d630ef34b397fbef4c2987ba2e66a9a36098ef88
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-12 14:32:01 +0200

    :clown_face: Add dummy question tree

--- src/infrastructure/config/index.ts
+++ src/infrastructure/config/index.ts
@@ -1,5 +1,42 @@
 import * as dotenv from "dotenv";
+import {
+    Answer,
+    ChoiceId,
+    Question,
+    QuestionTitle,
+} from "../../domain/support/question-tree";
 
 dotenv.config();
 
 export const port = process.env.PORT;
+
+const answerLevel1: Answer = {
+    content: "Content level 1",
+};
+const answerLevel2: Answer = {
+    content: "Content level 2",
+};
+export const questionTree: Question = {
+    title: "First question" as QuestionTitle,
+    choices: [
+        {
+            id: "choice1_1" as ChoiceId,
+            label: "Choice 1 1",
+            link: {
+                title: "Second question" as QuestionTitle,
+                choices: [
+                    {
+                        id: "choice2_1" as ChoiceId,
+                        label: "Choice 2 1",
+                        link: answerLevel2,
+                    },
+                ],
+            },
+        },
+        {
+            id: "choice1_2" as ChoiceId,
+            label: "Choice 1 2",
+            link: answerLevel1,
+        },
+    ],
+};
