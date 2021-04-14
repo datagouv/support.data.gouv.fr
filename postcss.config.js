535c3e560436529599c52cfb8908555f7d9ead4e
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-14 17:31:06 +0200

    :zap: Do not purge CSS in dev mode

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,18 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
 const tailwincss = require("tailwindcss");
 
+const devMode = process.env.NODE_ENV !== "production";
+
 module.exports = {
     plugins: [
         "postcss-import",
         "postcss-url",
         tailwincss({ purge: false }),
         "autoprefixer",
-        purgecss({
-            content: ["./views/**/*.njk"],
-        }),
+        devMode
+            ? ""
+            : purgecss({
+                  content: ["./views/**/*.njk"],
+              }),
     ],
 };
