d8c9eb2df9dae34faf3e26ff6a56d918561ec507
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-15 23:36:17 +0200

    :white_check_mark: Test content

--- src/infrastructure/config/question-tree.factory.test.ts
+++ src/infrastructure/config/question-tree.factory.test.ts
@@ -1,9 +1,18 @@
+import { Answer, Question } from "../../domain/support/question-tree";
 import { buildQuestionTree } from "./question-tree.factory";
 
 describe("The question tree factory", () => {
+    const functionToTest = () => buildQuestionTree(__dirname + "/__tests__");
+
     it("builds valid question trees", () => {
-        const functionToTest = () =>
-            buildQuestionTree(__dirname + "/__tests__");
         expect(functionToTest).not.toThrowError();
+    });
+
+    it("reads content from config file", () => {
+        const questionTree = functionToTest();
+        expect(
+            (((questionTree.choices[0].link as Question).choices[0]
+                .link as Question).choices[0].link as Answer).content
+        ).toEqual("answers/demandes-valeurs-foncieres.md");
     });
 });
