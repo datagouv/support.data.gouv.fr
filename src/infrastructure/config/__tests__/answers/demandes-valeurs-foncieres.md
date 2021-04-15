b1db2e52daf9a3fd85802663513674b5aa51bf92
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-15 23:32:30 +0200

    :recycle: Use test question tree

---
+++ src/infrastructure/config/__tests__/answers/demandes-valeurs-foncieres.md
@@ -0,0 +1,1 @@
+Réponse pour **demandes valeurs foncières**.
--- src/infrastructure/config/question-tree.factory.test.ts
+++ src/infrastructure/config/question-tree.factory.test.ts
@@ -2,7 +2,8 @@ import { buildQuestionTree } from "./question-tree.factory";
 
 describe("The question tree factory", () => {
     it("builds valid question trees", () => {
-        const functionToTest = () => buildQuestionTree();
+        const functionToTest = () =>
+            buildQuestionTree(__dirname + "/__tests__");
         expect(functionToTest).not.toThrowError();
     });
 });
