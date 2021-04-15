d2057897f4b214a0159908dc546d5d64ddd181b1
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-15 16:28:25 +0200

    :recycle: Move question type check to factory file

--- src/infrastructure/config/question-tree.factory.ts
+++ src/infrastructure/config/question-tree.factory.ts
@@ -1,6 +1,56 @@
 import * as fs from "fs";
 import YAML from "yaml";
-import { Question } from "../../domain/support/question-tree";
+import { Choice, Question } from "../../domain/support/question-tree";
+
+export const checkType = (questionTree: unknown): questionTree is Question => {
+    if (typeof questionTree !== "object" || !questionTree) {
+        return false;
+    }
+    if (!("title" in questionTree)) {
+        return false;
+    }
+    if (!("choices" in questionTree)) {
+        return false;
+    }
+    const candidate = questionTree as Question;
+    if (typeof candidate.title !== "string") {
+        return false;
+    }
+    if (!candidate.choices) {
+        return false;
+    }
+    if (!(candidate.choices instanceof Array)) {
+        return false;
+    }
+    return candidate.choices.reduce((acc: boolean, choice: unknown) => {
+        if (typeof choice !== "object" || !choice) {
+            return false;
+        }
+        if (!("id" in choice)) {
+            return false;
+        }
+        if (!("label" in choice)) {
+            return false;
+        }
+        if (!("link" in choice)) {
+            return false;
+        }
+        const candidate = choice as Choice;
+        if (!candidate.id) {
+            return false;
+        }
+        if (!candidate.label) {
+            return false;
+        }
+        if (!candidate.link) {
+            return false;
+        }
+        if ("content" in candidate.link) {
+            return acc && candidate.link.content !== undefined;
+        }
+        return acc && checkType(candidate.link);
+    }, true);
+};
 
 export const buildQuestionTree = (): Question => {
     const yamlFileContent = fs.readFileSync(
