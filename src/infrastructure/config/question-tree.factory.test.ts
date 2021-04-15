09c699427c7c3c4769588435a1fe4caa3a5b2f1e
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-15 23:31:13 +0200

    :recycle: Wrap factory function to test

--- src/infrastructure/config/question-tree.factory.test.ts
+++ src/infrastructure/config/question-tree.factory.test.ts
@@ -2,6 +2,7 @@ import { buildQuestionTree } from "./question-tree.factory";
 
 describe("The question tree factory", () => {
     it("builds valid question trees", () => {
-        expect(buildQuestionTree).not.toThrowError();
+        const functionToTest = () => buildQuestionTree();
+        expect(functionToTest).not.toThrowError();
     });
 });
