79553b102d04f01553deeb0d7fa5791c7c647a85
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-15 15:56:57 +0200

    :truck: Move question tree file to a config directory

--- src/infrastructure/config/index.ts
+++ src/infrastructure/config/index.ts
@@ -7,6 +7,6 @@ dotenv.config();
 
 export const port = process.env.PORT;
 
-const yamlFileContent = fs.readFileSync("question-tree.yaml", "utf8");
+const yamlFileContent = fs.readFileSync("config/question-tree.yaml", "utf8");
 
 export const questionTree = YAML.parse(yamlFileContent) as Question;
