54569951ca032d20a5bfec580dc6074db322d8a2
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-15 16:42:16 +0200

    :sparkles: Add raw question tree type guard

--- src/infrastructure/config/question-tree.factory.ts
+++ src/infrastructure/config/question-tree.factory.ts
@@ -2,12 +2,60 @@ import * as fs from "fs";
 import YAML from "yaml";
 import { Choice, Question } from "../../domain/support/question-tree";
 
+type RawChoice = {
+    label: string;
+    link: { content: string } | RawQuestionTree;
+};
+
 export type RawQuestionTree = {
     title: string;
-    choices: {
-        label: string;
-        link: { content: string } | RawQuestionTree;
-    }[];
+    choices: RawChoice[];
+};
+
+export const checkRawQuestionTreeType = (
+    rawQuestionTree: unknown
+): rawQuestionTree is RawQuestionTree => {
+    if (typeof rawQuestionTree !== "object" || !rawQuestionTree) {
+        return false;
+    }
+    if (!("title" in rawQuestionTree)) {
+        return false;
+    }
+    if (!("choices" in rawQuestionTree)) {
+        return false;
+    }
+    const candidate = rawQuestionTree as RawQuestionTree;
+    if (typeof candidate.title !== "string") {
+        return false;
+    }
+    if (!candidate.choices) {
+        return false;
+    }
+    if (!(candidate.choices instanceof Array)) {
+        return false;
+    }
+    return candidate.choices.reduce((acc: boolean, choice: unknown) => {
+        if (typeof choice !== "object" || !choice) {
+            return false;
+        }
+        if (!("label" in choice)) {
+            return false;
+        }
+        if (!("link" in choice)) {
+            return false;
+        }
+        const candidate = choice as RawChoice;
+        if (!candidate.label) {
+            return false;
+        }
+        if (!candidate.link) {
+            return false;
+        }
+        if ("content" in candidate.link) {
+            return acc && candidate.link.content !== undefined;
+        }
+        return acc && checkRawQuestionTreeType(candidate.link);
+    }, true);
 };
 
 export const checkQuestionTreeType = (
