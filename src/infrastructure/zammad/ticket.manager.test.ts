6b6eb456ec98951a2b6494dca56a77e97a4d8855
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-20 15:35:29 +0200

    :white_check_mark: Add empty test for zammad manager

---
+++ src/infrastructure/zammad/ticket.manager.test.ts
@@ -0,0 +1,7 @@
+import { ZammadTicketManager } from "./ticket.manager";
+
+describe("The Zammad ticket manager", () => {
+    it("exists", () => {
+        expect(ZammadTicketManager).toBeDefined();
+    });
+});
