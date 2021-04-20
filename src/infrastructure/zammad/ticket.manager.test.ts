498c60045ae993ecb5a9a491644a30c470fd425b
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-20 15:39:06 +0200

    :sparkles: Zammad manager uses zammad client

--- src/infrastructure/zammad/ticket.manager.test.ts
+++ src/infrastructure/zammad/ticket.manager.test.ts
@@ -1,7 +1,16 @@
 import { ZammadTicketManager } from "./ticket.manager";
 
 describe("The Zammad ticket manager", () => {
+    const clientMock = {
+        createUser: jest.fn(),
+        createTicket: jest.fn(),
+    };
     it("exists", () => {
         expect(ZammadTicketManager).toBeDefined();
+    });
+
+    it("uses the zammad client", () => {
+        const zammadTickerManager = new ZammadTicketManager(clientMock);
+        expect(zammadTickerManager).toBeDefined();
     });
 });
--- src/infrastructure/zammad/ticket.manager.ts
+++ src/infrastructure/zammad/ticket.manager.ts
@@ -1,7 +1,10 @@
 import { CreateTicketDTO } from "../../domain/ticketing/create-ticket.dto";
 import { TicketManager } from "../../domain/ticketing/ticket.manager";
+import { ZammadClient } from "./zammad.client";
 
 export class ZammadTicketManager implements TicketManager {
+    constructor(private readonly zammadClient: ZammadClient) {}
+
     createTicket(createTicket: CreateTicketDTO): Promise<void> {
         throw new Error("Method not implemented.");
     }
