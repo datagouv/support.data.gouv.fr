f270cd150f755b903824b3ca70b4a4e3ddc5ec06
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-20 12:02:59 +0200

    :recycle: Extract axios instance

--- src/infrastructure/zammad/zammad.client.test.ts
+++ src/infrastructure/zammad/zammad.client.test.ts
@@ -1,15 +1,24 @@
-import { ZammadClient } from "./zammad.client";
-import axios from "axios";
+process.env.ZAMMAD_BASE_URL = "https://zammad.croute/api";
+process.env.ZAMMAD_ACCESS_TOKEN = "croute";
 
-jest.mock("axios");
+import { axiosClient, ZammadClient } from "./zammad.client";
 
 describe("The Zammad client", () => {
     const client = new ZammadClient();
     const firstname = "Jean";
     const lastname = "Moust";
     const email = "jean@moust.fr";
-    process.env.ZAMMAD_ACCESS_TOKEN = "croute";
-    process.env.ZAMMAD_BASE_URL = "https://zammad.croute/api";
+    axiosClient.post = jest.fn();
+
+    it("uses an axios instance", () => {
+        expect(axiosClient).toBeDefined();
+        expect(axiosClient.defaults).toMatchObject({
+            baseURL: "https://zammad.croute/api",
+            headers: {
+                Authorization: "Bearer croute",
+            },
+        });
+    });
 
     it("exists", () => {
         expect(client).toBeDefined();
@@ -17,18 +26,11 @@ describe("The Zammad client", () => {
 
     it("uses axios under the hood", async () => {
         await client.createUser(firstname, lastname, email);
-        expect(axios.request).toBeCalledWith({
-            method: "post",
-            url: "https://zammad.croute/api/users",
-            headers: {
-                Authorization: "Bearer croute",
-            },
-            data: {
-                firstname,
-                lastname,
-                email,
-                roles: ["Customer"],
-            },
+        expect(axiosClient.post).toBeCalledWith("/users", {
+            firstname,
+            lastname,
+            email,
+            roles: ["Customer"],
         });
     });
 });
--- src/infrastructure/zammad/zammad.client.ts
+++ src/infrastructure/zammad/zammad.client.ts
@@ -1,23 +1,23 @@
 import axios from "axios";
 
+export const axiosClient = axios.create({
+    baseURL: process.env.ZAMMAD_BASE_URL,
+    headers: {
+        Authorization: `Bearer ${process.env.ZAMMAD_ACCESS_TOKEN}`,
+    },
+});
+
 export class ZammadClient {
     async createUser(
         firstname: string,
         lastname: string,
         email: string
     ): Promise<void> {
-        await axios.request({
-            method: "post",
-            url: `${process.env.ZAMMAD_BASE_URL}/users`,
-            headers: {
-                Authorization: `Bearer ${process.env.ZAMMAD_ACCESS_TOKEN}`,
-            },
-            data: {
-                firstname,
-                lastname,
-                email,
-                roles: ["Customer"],
-            },
+        await axiosClient.post("/users", {
+            firstname,
+            lastname,
+            email,
+            roles: ["Customer"],
         });
     }
 }
