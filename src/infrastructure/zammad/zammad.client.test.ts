816df0ed9b63670aa8a648825e889122c6f0c810
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-20 11:40:19 +0200

    :sparkles: Zammad client uses axios

--- src/infrastructure/zammad/zammad.client.test.ts
+++ src/infrastructure/zammad/zammad.client.test.ts
@@ -1,7 +1,25 @@
 import { ZammadClient } from "./zammad.client";
+import axios from "axios";
+
+jest.mock("axios");
 
 describe("The Zammad client", () => {
+    const client = new ZammadClient();
+    const firstname = "Jean";
+    const lastname = "Moust";
+    const email = "jean@moust.fr";
+
     it("exists", () => {
-        expect(ZammadClient).toBeDefined();
+        expect(client).toBeDefined();
+    });
+
+    it("uses axios under the hood", async () => {
+        await client.createUser(firstname, lastname, email);
+        expect(axios.post).toBeCalledWith("/users", {
+            firstname,
+            lastname,
+            email,
+            roles: ["Customer"],
+        });
     });
 });
--- src/infrastructure/zammad/zammad.client.ts
+++ src/infrastructure/zammad/zammad.client.ts
@@ -1,1 +1,16 @@
-export class ZammadClient {}
+import axios from "axios";
+
+export class ZammadClient {
+    async createUser(
+        firstname: string,
+        lastname: string,
+        email: string
+    ): Promise<void> {
+        await axios.post("/users", {
+            firstname,
+            lastname,
+            email,
+            roles: ["Customer"],
+        });
+    }
+}
