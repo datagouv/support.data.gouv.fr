904fc7ddc387e3040f86b7e9590223ab098c8364
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-21 16:14:16 +0200

    :white_check_mark: Add simple controller test

---
+++ src/presentation/controllers/support-ticket.controller.test.ts
@@ -0,0 +1,7 @@
+import { createSupportTicket } from "./support-ticket.controller";
+
+describe("The support ticket controller", () => {
+    it("exists", () => {
+        expect(createSupportTicket).toBeDefined();
+    });
+});
