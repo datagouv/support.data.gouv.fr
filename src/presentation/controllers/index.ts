99643d357799c0603e8731d6a7c14da2bccf0335
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-21 17:49:03 +0200

    :sparkles: Add route for form post

--- src/presentation/controllers/index.ts
+++ src/presentation/controllers/index.ts
@@ -1,6 +1,8 @@
 import { Application } from "express";
 import { navigateSupportFlow } from "./support-flow.controller";
+import { createSupportTicket } from "./support-ticket.controller";
 
 export const attachControllersToApp = (app: Application): void => {
+    app.post("/tickets", createSupportTicket);
     app.get("/(*)?", navigateSupportFlow);
 };
