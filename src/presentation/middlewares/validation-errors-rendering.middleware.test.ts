98028fab63e293d2930760cbf46582211c797452
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-27 17:36:45 +0200

    :sparkles: Validation errors rendering middleware renders the turbo stream

--- src/presentation/middlewares/validation-errors-rendering.middleware.test.ts
+++ src/presentation/middlewares/validation-errors-rendering.middleware.test.ts
@@ -1,15 +1,49 @@
 import { Request, Response } from "express";
+import { ValidationError } from "yup";
 import { renderValidationErrors } from "./validation-errors-rendering.middleware";
 
 describe("The validation errors rendering middleware", () => {
+    const req = {
+        body: Symbol(),
+    } as Request;
+    const res = ({
+        render: jest.fn(),
+        status: jest.fn(),
+        type: jest.fn(),
+    } as unknown) as Response;
+    const next = jest.fn();
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
     it("does nothing if there is no validation error", () => {
         const error = undefined;
-        const req = (jest.fn() as unknown) as Request;
-        const res = (jest.fn() as unknown) as Response;
-        const next = jest.fn();
 
         renderValidationErrors(error, req, res, next);
 
         expect(next).toBeCalledWith();
+    });
+
+    it("renders the turbo streams with validation errors", () => {
+        const error = new ValidationError([
+            new ValidationError(
+                "c'est pas valide frère",
+                "pas valide",
+                "croute"
+            ),
+        ]);
+
+        renderValidationErrors(error, req, res, next);
+
+        expect(next).not.toBeCalled();
+        expect(res.status).toHaveBeenCalledWith(422);
+        expect(res.type).toHaveBeenCalledWith("text/vnd.turbo-stream.html");
+        expect(res.render).toBeCalledWith("includes/form/stream.njk", {
+            userInput: req.body,
+            error: {
+                fieldsInError: ["croute"],
+            },
+        });
     });
 });
--- src/presentation/middlewares/validation-errors-rendering.middleware.ts
+++ src/presentation/middlewares/validation-errors-rendering.middleware.ts
@@ -1,10 +1,40 @@
 import { NextFunction, Request, Response } from "express";
+import { ValidationError } from "yup";
 
 export const renderValidationErrors = (
-    err: Error | undefined,
+    error: Error | undefined,
     req: Request,
     res: Response,
     next: NextFunction
 ): void => {
-    next();
+    if (!(error instanceof ValidationError)) {
+        next();
+        return;
+    }
+    res.render("includes/form/stream.njk", {
+        userInput: req.body,
+        error: presentValidationError(error),
+    });
+    res.status(422);
+    res.type("text/vnd.turbo-stream.html");
+};
+
+type FieldsValidationError = {
+    fieldsInError: string[];
+};
+
+export const presentValidationError = (
+    validationError: ValidationError
+): FieldsValidationError => {
+    return validationError.inner.reduce(
+        (errorMap, error) => {
+            if (error.path == undefined) {
+                return errorMap;
+            }
+            return {
+                fieldsInError: [...errorMap.fieldsInError, error.path],
+            };
+        },
+        { fieldsInError: [] } as FieldsValidationError
+    );
 };
