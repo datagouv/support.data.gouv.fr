9a231875f405e11394a0c6e5d3febbc4ad8f1d2b
Stanislas Bernard <bernardstanislas@gmail.com>
2021-05-04 11:46:01 +0200

    :sparkles: Compute user path

--- src/application/presenters/support-flow.presenter.ts
+++ src/application/presenters/support-flow.presenter.ts
@@ -3,12 +3,22 @@ import { SupportFlow, UserChoices } from "../../domain/support/support-flow";
 export const supportFlowPresenter = (
     supportFlow: SupportFlow,
     userChoices: UserChoices
-): { supportFlow: SupportFlow; choicesPrefixes: string[] } => {
+): {
+    supportFlow: SupportFlow;
+    choicesPrefixes: string[];
+    userPath: string;
+} => {
     const choicesPrefixes = userChoices.map((_, index) =>
         userChoices.slice(0, index + 1).join("/")
     );
+    const userPath = supportFlow.levels
+        .map((level) => level.choices.find((choice) => choice.selected)?.label)
+        .filter((label) => !!label)
+        .join(" |> ");
+
     return {
         supportFlow,
         choicesPrefixes,
+        userPath,
     };
 };
