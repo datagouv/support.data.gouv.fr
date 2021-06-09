1a9e1f48af1274e2b5d65a437fae60bbd2651776
Stanislas Bernard <bernardstanislas@gmail.com>
2021-06-09 14:11:51 +0200

    :recycle: Change type guard using zod

--- src/infrastructure/config/raw-question-tree.guard.ts
+++ src/infrastructure/config/raw-question-tree.guard.ts
@@ -1,61 +1,25 @@
-import { RawChoice, RawQuestionTree } from "./question-tree.factory";
+import { RawQuestionTree } from "./question-tree.factory";
+import { z } from "zod";
+
+const rawQuestionTreeSchema: z.Schema<RawQuestionTree> = z.object({
+  title: z.string(),
+  choices: z.array(
+    z.object({
+      label: z.string(),
+      link: z.union([
+        z.object({ content: z.string() }),
+        z.object({ path: z.string() }),
+        z.object({
+          form: z.object({ title: z.string(), recipient: z.string() }),
+        }),
+        z.lazy(() => rawQuestionTreeSchema),
+      ]),
+    })
+  ),
+});
 
 export const checkRawQuestionTreeType = (
-    rawQuestionTree: unknown
+  rawQuestionTree: unknown
 ): rawQuestionTree is RawQuestionTree => {
-    if (typeof rawQuestionTree !== "object" || !rawQuestionTree) {
-        return false;
-    }
-    if (!("title" in rawQuestionTree)) {
-        return false;
-    }
-    if (!("choices" in rawQuestionTree)) {
-        return false;
-    }
-    const candidate = rawQuestionTree as RawQuestionTree;
-    if (typeof candidate.title !== "string") {
-        return false;
-    }
-    if (!candidate.choices) {
-        return false;
-    }
-    if (!(candidate.choices instanceof Array)) {
-        return false;
-    }
-    return candidate.choices.reduce((acc: boolean, choice: unknown) => {
-        if (typeof choice !== "object" || !choice) {
-            return false;
-        }
-        if (!("label" in choice)) {
-            return false;
-        }
-        if (!("link" in choice)) {
-            return false;
-        }
-        const candidate = choice as RawChoice;
-        if (!candidate.label) {
-            return false;
-        }
-        if (!candidate.link) {
-            return false;
-        }
-        if ("content" in candidate.link) {
-            return acc && candidate.link.content !== undefined;
-        }
-        if ("path" in candidate.link) {
-            return acc && candidate.link.path !== undefined;
-        }
-        if (
-            "form" in candidate.link &&
-            "title" in candidate.link.form &&
-            "recipient" in candidate.link.form
-        ) {
-            return (
-                acc &&
-                candidate.link.form.title !== undefined &&
-                candidate.link.form.recipient !== undefined
-            );
-        }
-        return acc && checkRawQuestionTreeType(candidate.link);
-    }, true);
+  return rawQuestionTreeSchema.safeParse(rawQuestionTree).success;
 };
